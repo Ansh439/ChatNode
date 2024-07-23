cbb49851d34d8956571e5c3f39cbb96d1be15953
Ansh439 <gupta.ansh3150@gmail.com>
2024-07-23 11:35:45 +0530

    initial setups done & mongoose connected

--- server/index.js
+++ server/index.js
@@ -1,7 +1,30 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import mongoose from 'mongoose';
+
+dotenv.config();
 
 const app = express();
 
-app.listen('8080', () => {
-    console.log("Server starts running at port 8080");
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(
+        () => {
+            console.log("mongoose Connected!");
+        }
+    )
+    .catch((error) => {
+        console.log(error);
+    })
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}))
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server starts running at port ${PORT}`);
 })
